56d8318170621874eb5bef7a57572db41ca719c3
Wish_Time <wish-time@outlook.com>
2018-01-03 11:05:56 +0800

    Initial commit

---
+++ skin/js/nav.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function() {	
+$(".nav ul li").hover(function() { 
+	$(this).addClass("on");
+} , function() { 
+	$(this).removeClass("on");
+});
+	
+});
+
+
